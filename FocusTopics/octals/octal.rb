c7414c2410b83b3aa0a72d93ee64354dd67fc286
Carlo Santos <carlo@snapit.solutions>
2020-08-25 14:47:01 -0500

    adding octal file

---
+++ FocusTopics/octals/octal.rb
@@ -0,0 +1,13 @@
+=begin
+  We will need to be able to convert
+  a decimal to an octal. [https://www.youtube.com/watch?v=ayul1fmZd0Y]
+
+  Will we also need to convert from
+  octal to decimal? [https://www.youtube.com/watch?v=YCM2JReWS10]
+  ?
+=end
+
+# 394 base 10 -> 612 base 8
+# 784 base 10 -> 1420 base 8
+# 120 base 10 -> 170 base 8
+
